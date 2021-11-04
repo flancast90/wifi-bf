2b816ca25d8b64548b37577ef82239599904a52a
Finn Lancaster <flancast90@gmail.com>
2021-11-04 12:09:42 -0400

    add support for wifi security returns

--- src/ssid.py
+++ src/ssid.py
@@ -3,17 +3,27 @@ import subprocess
 def start(code):
 	r = subprocess.run(["nmcli", "-f", "SSID", "dev", "wifi"], capture_output=True, text=True).stdout
 	grep = r.split("\n")
+	
+	s = subprocess.run(["nmcli", "-f", "SECURITY", "dev", "wifi"], capture_output=True, text=True).stdout
+	grep_s = s.split("\n")
 
 	networks = [k.strip() for k in grep if (k.strip() != "SSID") and (k.strip() != "--") and (k.strip() != "")]
+	net_type = [k.strip() for k in grep_s if (k.strip() != "SECURITY") and (k.strip() != "")]
+	
 
 	ssid = []
+	security = []
 
-	filtered = [ssid.append(networks[i]) for i in range(len(networks)) if networks[i] not in ssid]
+	for i in range(len(net_type)):
+		if networks[i] not in ssid:
+			ssid.append(networks[i])
+			security.append(net_type[i])
 	
 	if (code == 0):
 		print(ssid)
+		print(security)
 	else:
-		return ssid
+		return [ssid, security]
 		
 if __name__ == "__main__":
 	start(0)
